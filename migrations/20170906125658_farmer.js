b3bcf13b86374a7fbf954f4b44814c6bb44f5e5b
Ross Kiser <kiserr10@gmail.com>
2017-09-06 15:40:59 -0600

    market and location migrations added

--- migrations/20170906125658_farmer.js
+++ migrations/20170906125658_farmer.js
@@ -2,7 +2,8 @@
 exports.up = function(knex, Promise) {
 	return knex.schema.createTable('farmer', (table) => {
 		table.increments('id').primary();
-		table.integer('user_id').unsigned().references('id').inTable('user').onDelete('cascade');
+		table.integer('user_id').unsigned();
+		table.foreign('user_id').references('id').inTable('user').onDelete('cascade');
 		table.text('biography').notNull();
 		table.text('farm_name').notNull();
 		table.text('image_url');
---
+++ migrations/20170906141655_market.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+	return knex.schema.createTable('market', (table) =>{
+		table.increments('id').primary();
+		table.integer('location_id').unsigned();
+		table.foreign('location_id').references('id').inTable('location').onDelete('cascade');
+		table.text('description').notNull();
+		table.text('name').notNull();
+		table.text('image_url');
+		table.float('rating');
+	});
+};
+
+exports.down = function(knex, Promise) {
+	return knex.schema.dropTableIfExists('market');
+};
