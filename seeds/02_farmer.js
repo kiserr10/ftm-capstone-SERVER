239c30897dc5e277e002bfe23c4e9be8c3be52ce
Ross Kiser <kiserr10@gmail.com>
2017-09-06 21:16:46 -0600

    second seed file created and ran successfully

---
+++ seeds/02_farmer.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex, Promise) {
+	return knex('farmer').del()
+		.then(function () {
+			return knex('farmer').insert([
+				{
+					id: 1,
+					account_id: 2,
+					biography: 'For over two decades Waller Farms has been at the pinnacle of tomato harvesting.',
+					farm_name: 'Waller Farms',
+					image_url: 'http://images.all-free-download.com/images/graphicthumb/tomato_plant_red_218370.jpg',
+					rating: 10
+				},
+				{
+					id: 2,
+					account_id: 4,
+					biography: 'For over two decades Waller Farms has been at the pinnacle of carrot harvesting.',
+					farm_name: 'Holter Farms',
+					image_url: 'http://images.all-free-download.com/images/graphicthumb/tomato_plant_red_218370.jpg',
+					rating: 10
+				},
+				{
+					id: 3,
+					account_id: 3,
+					biography: 'For over two decades Waller Farms has been at the pinnacle of lettuce harvesting.',
+					farm_name: 'Deere Farms',
+					image_url: 'http://images.all-free-download.com/images/graphicthumb/tomato_plant_red_218370.jpg',
+					rating: 10
+				}
+			]);
+		});
+};
